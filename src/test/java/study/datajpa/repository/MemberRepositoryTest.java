0897839aee23eb2e06555b56473e3ddb1e1b5016
chanhl22 <chanhlee@student.42seoul.kr>
2022-04-14 15:02:36 +0900

    JPA와 DB 설정 및 동작 테스트

---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,37 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+//@Rollback(false)
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member("memberA");
+
+        //when
+        Member savedMember = memberRepository.save(member);
+        Optional<Member> byId = memberRepository.findById(savedMember.getId());
+        Member findMember = byId.get();
+
+        //then
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember).isEqualTo(member);
+    }
+
+}
